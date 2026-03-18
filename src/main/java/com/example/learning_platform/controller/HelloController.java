6b4cc71641d70559ee4348bf1d8d19074fd9cb89
Nick <lolipop22234@gmail.com>
2026-03-18 20:08:22 +0200

    Reorganize project, add controller, service, repositore, model packages

--- src/main/java/com/example/learning_platform/controller/HelloController.java
+++ src/main/java/com/example/learning_platform/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.learning_platform;
+package com.example.learning_platform.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
